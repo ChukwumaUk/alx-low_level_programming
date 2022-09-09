64b934846ed11bb6df5afd76212a912bb677399a
Chukwuma <chukwumaukaha@gmail.com>
2022-09-09 11:34:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *Writing a program that prints a statement with the puts function
+ *
+ */
+
+int main(void)
+{
+
 puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+}
