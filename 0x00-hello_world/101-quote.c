b72f7df6f1c286f17dfd3fc39db3aaedca686443
Chukwuma <chukwumaukaha@gmail.com>
2022-09-09 14:18:41 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Printing a statement without printf or put
+ *
+ * Return: Return 1 in this program
+ */
+
+int main(void)
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+return (0);
+
+}
