7fa618a03bc61efadbba2e6c58d910d4d04a679d
Chukwuma <chukwumaukaha@gmail.com>
2022-09-09 12:54:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using the printf option to print
+ *
+ * Return: Program will return 0(success)
+ */
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+}
