8dda3a15c28a3e3241538764c6aaffcd2b0239e6
Chukwuma <chukwumaukaha@gmail.com>
2022-09-10 16:42:37 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print decimals with putchar
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+
+int num;
+
+for (num = 0; num < 10; num++)
+	putchar(num);
+putchar('\n');
+return (0);
+
+}
