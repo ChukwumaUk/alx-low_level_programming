e511c47ba2d5257b91e5b1a9c6294e8144c3c3ba
Chukwuma <chukwumaukaha@gmail.com>
2022-09-13 16:18:17 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10x the alphabet in lowercase
+ *
+ * Description: Print 10x alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+
+char c;
+int i = 0;
+
+while (i++ <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+
+_putchar('\n');
+}
+}
+
+
+}
