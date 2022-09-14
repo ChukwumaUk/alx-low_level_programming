cb05532f2ceff9c13cbc06e17017ea06daef1f19
Chukwuma <chukwumaukaha@gmail.com>
2022-09-13 21:06:40 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the result
+ *
+ * @int: numbers to be added
+ *
+ * Return: void
+ */
+
+int add(int, int)
+{
+
+int result;
+int num1, num2;
+
+result = num1 + num2;
+
+putchar(result);
+putchar('\n');
+
+}
