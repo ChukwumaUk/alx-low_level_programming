19965c69eabbb891be0e6ec1e4749acd510743ae
Chukwuma <chukwumaukaha@gmail.com>
2022-09-13 19:19:34 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of number
+ *
+ * @n: number to check
+ *
+ * Return: void
+ */
+
+int print_last_digit(int n)
+{
+
+int n, digit;
+
+digit = n % 10;
+putchar(digit);
+return (0);
+
+}
