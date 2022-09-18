ded39b032c15cda6c3211d31ec8d2545d8cecc9b
Chukwuma <chukwumaukaha@gmail.com>
2022-09-18 09:48:49 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Check for uppercase character
+ *
+ * @c: The character to be checked
+ *
+ * Return: Returns 1 if c is uppercase else, 0
+ */
+
+int _isupper(int c)
+{
+
+for (c = 'A'; c <= 'Z'; c++)
+return (1);
+
+else
+	return (0);
+
+}
