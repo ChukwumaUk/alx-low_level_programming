e697b2cc85943f859a3a71cf9d1f7b2b4458069b
Chukwuma <chukwumaukaha@gmail.com>
2022-09-18 10:42:16 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print nos 0 to 9
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+
+int dig;
+
+for (dig = 0; dig <= 9; dig++)
+{
+_putchar((dig % 10) + '0');
+_putchar('\n');
+}
+
+}
