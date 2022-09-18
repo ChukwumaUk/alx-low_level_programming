64a4d68cffc44405826cfa21f496d9dd73e53cb6
Chukwuma <chukwumaukaha@gmail.com>
2022-09-18 15:40:25 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Print a square followed by a newline
+ *
+ * @size: size of the square
+ *
+ * Return - void
+ */
+
+void print_square(int size)
+{
+int hgt, wid;
+
+if (size > 0)
+{
+for (hgt = 0; hgt < size; hgt++)
+{
+for (wid = 0; wid < size; wid++)
+	_putchar('#');
+
+if (hgt == size - 1)
+	continue;
+_putchar('\n');
+
+}
+
+}
+_putchar('\n');
+}
