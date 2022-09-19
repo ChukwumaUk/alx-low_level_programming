e9eaad73d5b0297f1d6e083010d07188e75b5b59
Chukwuma <chukwumaukaha@gmail.com>
2022-09-19 16:11:41 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @s: string to print
+ *
+ * Return: String in reverse order
+ */
+
+void print_rev(char *s)
+{
+
+int len = 0, list;
+
+while (s[list++])
+	len++
+
+		for (list = len - 1; list >= 0; list--)
+			_putchar(s[list]);
+_putchar('\n');
+
+}
