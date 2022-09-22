e4411b01e412a37fbbb3a121c6e3f761dda9b79b
Chukwuma <chukwumaukaha@gmail.com>
2022-09-21 19:09:38 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 
-int i = 0, j = 0;
+int index = 0, dest_len = 0;
 
-while (dest[i++])
-	j++;
+while (dest[index++])
+	dest_len++;
 
-for (i = 0; src[i]; i++)
-	dest[j] = src[i];
+for (index = 0; src[index]; index++)
+	dest[dest_len++] = src[index];
 
 	return (dest);
 
