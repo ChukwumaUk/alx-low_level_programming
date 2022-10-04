822f40696a44b20030f55d03ad13b6467d691f8b
Chukwuma <chukwumaukaha@gmail.com>
2022-10-04 07:56:53 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the number of
+ * arguments passed into it.
+ * @argc: Number of arguments passed to the function
+ * @argv: One dimensional array of strings
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+
+return (0);
+}
