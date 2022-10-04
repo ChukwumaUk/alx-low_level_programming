2e1116b3f70a8991236cdc315bff35ff597d8acb
Chukwuma <chukwumaukaha@gmail.com>
2022-10-04 07:32:38 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ *
+ * @argc: The number of arguments passed to the function
+ * @argv: One dimensional array of strings
+ *
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}
