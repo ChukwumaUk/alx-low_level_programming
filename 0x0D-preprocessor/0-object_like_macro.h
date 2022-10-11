9c932ebd7b9fdb2d6e7709b1c3ec22d87fb3c407
Chukwuma <chukwumaukaha@gmail.com>
2022-10-10 22:20:27 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,3 +1,13 @@
-#include "main.h"
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * main - Creating header file 0-object_like_macro.h
+ *
+ * Description: Create a header file that defines
+ * a macro named SIZE as an abbreviation for the token 1024.
+ */
 
 #define SIZE 1024
+
+#endif
