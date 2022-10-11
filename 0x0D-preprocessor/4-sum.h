447750941e40f2053f59122bc0c96bddbdca8a72
Chukwuma <chukwumaukaha@gmail.com>
2022-10-10 23:07:56 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * main - Write a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
+ * @x: First value to be computed
+ * @y: Second value to be computed
+ *
+ * Return: void
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
