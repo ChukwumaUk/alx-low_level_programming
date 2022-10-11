98057b708cec394c7759e457f68db3c0976d8e11
Chukwuma <chukwumaukaha@gmail.com>
2022-10-10 22:27:49 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * main - A header file for 1-pi.h
+ *
+ * Description: Create a header file that defines a macro
+ * named PI as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
