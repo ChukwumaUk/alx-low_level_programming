311ac092252f1002da757f7093bc234a5b596e0d
Chukwuma <chukwumaukaha@gmail.com>
2022-10-13 07:14:16 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - Write a function that prints a struct dog
+ *
+ * @d: Input pointer to new struct
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+if (d)
+{
+printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+printf("Age: %.6f\n", age->age);
+printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
+}
+}
