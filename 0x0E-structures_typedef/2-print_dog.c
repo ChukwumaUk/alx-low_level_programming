25aa1069424c5eda41e4f3be055e0c587cf5c580
Chukwuma <chukwumaukaha@gmail.com>
2022-10-13 07:19:08 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 if (d)
 {
 printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
-printf("Age: %.6f\n", age->age);
+printf("Age: %.6f\n", d->age);
 printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 }
 }
